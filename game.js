aa53eb285a4ecf1169c582a5b6faf06ccc03c937
TannorWallace <tannor.wallace@gmail.com>
2019-06-14 15:36:41 -0600

    first-commit

---
+++ game.js
@@ -0,0 +1,12 @@
+//Vengful spirit defenses
+
+let voidSheilds = 500;
+let armorPlate = 1000;
+let repelBoarders = 250;
+
+// loyalist attack variables
+
+let lazCannons = 150;
+let gunBattery = 350;
+let boardingTorpedos = 200;
+
