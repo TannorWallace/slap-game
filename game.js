1d1e8b8c607ee1c028653a9a8035261ebf211e87
TannorWallace <tannor.wallace@gmail.com>
2019-06-14 16:10:22 -0600

    FUNCTIONNNN!!!!!!!!

--- game.js
+++ game.js
@@ -1,12 +1,37 @@
 //Vengful spirit defenses
+//I want the defenses to be decreced by a certain amount for each volley
 
-let voidSheilds = 500;
-let armorPlate = 500;
-let repelBoarders = 250;
+let voidSheilds = 1250;
 
 // loyalist attack variables
+// i want a set number of munitions 
+//10 laz
+// 5 gun batteries
+// 6 boarding torpedos 
+// all to go down by 1 when used.
 
+// damage to be done
 let lazCannons = 150;
 let gunBattery = 350;
 let boardingTorpedos = 200;
 
+// Do I set values for the munitions?
+
+function attack() {
+  voidSheilds -= lazCannons
+  console.log(voidSheilds)
+}
+
+
+function gunVolley() {
+  voidSheilds -= gunBattery
+  console.log(voidSheilds)
+
+}
+
+
+function torpedos() {
+  voidSheilds -= boardingTorpedos
+  console.log(voidSheilds)
+
+}
