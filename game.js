3f95a9e79a01d5096f91c01650d0120cdd624616
TannorWallace <tannor.wallace@gmail.com>
2019-06-15 23:30:38 -0600

    UGH! I cannot figure out how to decrement the health bar or cycle images when the ship is taking damage

--- game.js
+++ game.js
@@ -2,22 +2,36 @@
 //I want the defenses to be decreced by a certain amount for each volley
 
 let health = 3250;
+
 let shipHealth = document.getElementById("health")
 
-// let armorPlate = 500;
+// let lazCannons = 10;
+// let gunBattery = 50;
+// let boardingTorpedos = 100;
 
-// let repelBoarders = 500;
+
+// function update() {
+//   console.log(health)
+//   if (health <= 2200) {
+//     shipHealth.innerHTML = "Void Shields Destroyed!!!"
+//   }
+//   if (health <= 1700) {
+//     shipHealth.innerHTML = "Armor Destroyed!!!"
+//   }
+//   if (health <= 900) {
+//     shipHealth.innerHTML = "Victory is close Brothers!!!"
+//   }
+//   if (health <= 300) {
+//     shipHealth.innerHTML = "Press the attack!!!"
+//   }
+//   if (health == 0)
+//     shipHealth.innerHTML = "Ship Destroyed Glory to the EMPEROR!!!"
+// }
+// console.log(update())
 
 
+// let shipHealth = document.getElementById("health")
 
-// loyalist attack variables
-// i want a set number of munitions 
-//10 laz
-// 5 gun batteries
-// 6 boarding torpedos 
-// all to go down by 1 when used.
-
-// health to be done
 let lazCannons = 10;
 let gunBattery = 50;
 let boardingTorpedos = 100;
@@ -25,6 +39,7 @@ let boardingTorpedos = 100;
 // Do I set values for the munitions?
 function update() {
   document.getElementById('health').innerText
+
 }
 
 function lazAttack() {
